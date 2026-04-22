31df21f53795c7874e6c94ffba6758381ea0d136
sebaduocccc <se.saavedrag@duocuc.cl>
2026-04-21 23:09:03 -0400

    Security basic user system Impl

---
+++ src/main/java/com/project/Orion/Config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.project.Orion.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        UserDetails regularUser = User.builder()
+                .username("usuario")
+                .password(passwordEncoder.encode("1234"))
+                .roles("USER")
+                .authorities("READ")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("ADMIN") 
+                .authorities("ROLE_ADMIN","READ","POST","UPDATE","DELETE")
+                .build();
+
+
+
+        return new InMemoryUserDetailsManager(regularUser, adminUser);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
--- src/main/java/com/project/Orion/Controller/HelloController.java
+++ src/main/java/com/project/Orion/Controller/HelloController.java
@@ -1,16 +1,33 @@
 package com.project.Orion.Controller;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@PreAuthorize("denyAll()")
 public class HelloController {
 
     @GetMapping
-    public String Hello() {
+    @PreAuthorize("hasAuthority('READ')")
+    public String HelloGet() {
         return "Hello World";
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('POST') or hasAuthority('READ')")
+    public String HelloPost() {
+        return "Hello World posteado";
+    }
+
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAnyRole('ADMIN','DEVELOPER')")
+    public String HelloAdmin() {
+        return "Hello World admin || developer";
     }
 }
