8b2089ffccbe187b1d28b050f5e74d18d5dd0c6e
sebaduocccc <se.saavedrag@duocuc.cl>
2026-04-25 18:13:35 -0400

    UsuarioService conectado a db

--- Usuario_Service/src/main/java/com/orion/Usuarios/Model/Usuario.java
+++ Usuario_Service/src/main/java/com/orion/Usuarios/Model/Usuario.java
@@ -1,4 +1,17 @@
 package com.orion.Usuarios.Model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
