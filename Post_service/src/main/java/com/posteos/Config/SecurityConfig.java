3c16f7272f87133221ac83d7b658cd4dac6d5da1
Maeton <MAteo2nogueira@gmail.com>
2026-04-26 18:23:00 +0000

    Request auth

--- Post_service/src/main/java/com/posteos/Config/SecurityConfig.java
+++ Post_service/src/main/java/com/posteos/Config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
 
-                    auth.anyRequest().permitAll();
+                    auth.anyRequest().authenticated();
                 })
                 .addFilterBefore(jwtValidationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
