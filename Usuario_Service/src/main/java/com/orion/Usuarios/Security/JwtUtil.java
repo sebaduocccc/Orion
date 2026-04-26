6b47b08506b0dfe3b2f1735a0e499c37985ca368
sebaduocccc <se.saavedrag@duocuc.cl>
2026-04-26 03:13:38 -0400

    roles agregado al token jwt

--- Usuario_Service/src/main/java/com/orion/Usuarios/Security/JwtUtil.java
+++ Usuario_Service/src/main/java/com/orion/Usuarios/Security/JwtUtil.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -44,6 +45,12 @@ public class JwtUtil {
 
 
     public String generateToken(Map<String,Object> extraClaims, UserDetails userDetails ) {
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(authority -> authority.getAuthority())
+                .toList();
+
+        extraClaims.put("roles", roles);
 
         return Jwts.builder()
                 .setClaims(extraClaims)
