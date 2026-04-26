b9602fbe20dd88c8c20cb31fe189ad6483d9673f
sebaduocccc <se.saavedrag@duocuc.cl>
2026-04-26 02:56:11 -0400

    jwt integrado en login

--- Usuario_Service/src/main/java/com/orion/Usuarios/Config/SecurityConfig.java
+++ Usuario_Service/src/main/java/com/orion/Usuarios/Config/SecurityConfig.java
@@ -3,10 +3,13 @@ package com.orion.Usuarios.Config;
 
 import com.orion.Usuarios.Entity.Usuario;
 import com.orion.Usuarios.Repository.UsuarioRepository;
+import com.orion.Usuarios.Security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +21,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,20 +34,30 @@ public class SecurityConfig {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
                 .csrf(csrf -> csrf.disable())
-                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/api/usuarios/registro").permitAll();
+                    auth.requestMatchers("/api/auth/login","/api/usuarios/registro").permitAll();
                     auth.anyRequest().authenticated();
                 })
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+
+        return config.getAuthenticationManager();
+
+    }
 
 
 
---
+++ Usuario_Service/src/main/java/com/orion/Usuarios/DTO/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.orion.Usuarios.DTO;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
