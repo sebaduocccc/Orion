a1bbea746d20514b85800c1b5b86496f6186e739
sebaduocccc <se.saavedrag@duocuc.cl>
2026-05-04 10:04:10 -0400

    agregado boton de logout

--- orion-app/src/App.jsx
+++ orion-app/src/App.jsx
@@ -2,24 +2,38 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./components/Home";
 
+const PrivateRoute = ({ children }) => {
+
+  const token = localStorage.getItem("token");
+
+  return token ? children : <Navigate to="/login" />; {/*Si el token existe, renderiza los hijos (la ruta protegida), de lo contrario redirige a /login*/}
+
+};
+
+
 function App() {
   return(
 
     <BrowserRouter>
-      <div className="bg-light min-vh-100">
         <Routes>
 
-          {/* redirigir raiz al /login que es declarado mas abajo */}
-          <Route path="/" element={<Navigate to="/login" />} />
-
-          {/* declarado el endpoint de /login */}
           <Route path="/login" element={<Login />} />
 
-          {/* declarado el endpoint de /home */}
-          <Route path="/home" element={<Home />} />
+          <Route 
+            path="/home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+
+
+
+          <Route path="*" element={<Navigate to="/login" />} /> {/*Redirige cualquier ruta no definida a /login*/}  
+  
 
         </Routes>
-      </div>
     </BrowserRouter>
 
   );
--- orion-app/src/components/Navbar.jsx
+++ orion-app/src/components/Navbar.jsx
@@ -1,12 +1,32 @@
-
+import { Link, useNavigate } from "react-router-dom";
 const NavBar = () => {
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+
+        localStorage.removeItem("token"); // Elimina el token del almacenamiento local
+        localStorage.clear(); // Limpia todo el almacenamiento local (opcional, si quieres eliminar otros datos relacionados)
+        navigate("/login"); // Redirige al usuario a la página de login
+
+    }
+
 
     return(
     <nav className="navbar navbar-dark bg-dark mb-4">
         <div className="container">
-            <a className="navbar-brand" href="/">
-                Orion
-            </a>
+            <Link className="navbar-brand" to="/home">
+            Orion App
+            </Link>
+
+
+            <div className="d-flex">
+
+                <button className="btn btn-outline-light" onClick={handleLogout}>
+                    Logout
+                </button>
+
+            </div>
         </div>
     </nav>
     );
