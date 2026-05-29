5de2ed38e45f79359c34d5bb6b0b717e47697bb7
sebaduocccc <se.saavedrag@duocuc.cl>
2026-05-28 22:46:57 -0400

    init eureka

---
+++ README.md
@@ -0,0 +1,1 @@
+ola mundos
---
+++ backend/user-service/src/main/java/com/treeaxes/userservice/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.treeaxes.userservice.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+
+    @GetMapping
+    public String sayHello(){
+        return "Hello World";
+    }
+
+}
