d85820f333ce3cf8e543118938e438a1a93d0cb1
sebaduocccc <se.saavedrag@duocuc.cl>
2026-06-23 20:55:06 -0400

    liquibase media-service arreglado 100%

--- MediaService/src/main/java/com/orion/mediaservice/Config/DataInitializer.java
+++ MediaService/src/main/java/com/orion/mediaservice/Config/DataInitializer.java
@@ -2,35 +2,29 @@ package com.orion.mediaservice.Config;
 
 import com.orion.mediaservice.Entity.Media;
 import com.orion.mediaservice.Repository.MediaRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.event.EventListener;
 
 import java.util.Set;
 
 @Configuration
+@RequiredArgsConstructor
 public class DataInitializer {
 
-    @Bean
-    @DependsOn("liquibase")
-    public CommandLineRunner init(MediaRepository mediaRepository){
-        return args -> {
+    private final MediaRepository mediaRepository;
 
-            if(mediaRepository.count() == 0){
-
-                Media media = new Media();
-                media.setNombreGenerado("default_avatar.png");
-                media.setNombreOriginal("default_avatar.png");
-                media.setTipo(Media.TipoMedia.AVATAR);
-                media.setUrlAcceso("/api/media/default_avatar.png");
-                media.setUserId(null);
-
-                mediaRepository.saveAll(Set.of(media));
-
-            }
-
-        };
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        if (mediaRepository.count() == 0) {
+            Media media = new Media();
+            media.setNombreGenerado("default_avatar.png");
+            media.setNombreOriginal("default_avatar.png");
+            media.setTipo(Media.TipoMedia.AVATAR);
+            media.setUrlAcceso("/api/media/default_avatar.png");
+            media.setUserId(null);
+            mediaRepository.saveAll(Set.of(media));
+        }
     }
-
 }
