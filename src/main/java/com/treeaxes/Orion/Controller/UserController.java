0cf1d2edc1b422f0a1daaee46d6cf6a1c2cf6371
RafaxWolf <rafalisandroflores@gmail.com>
2026-08-12 02:20:03 -0400

    Iniciando creacion de los usuarios

---
+++ src/main/java/com/treeaxes/Orion/Controller/UserController.java
@@ -0,0 +1,13 @@
+package com.treeaxes.Orion.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/usuarios")
+public class UserController {
+    
+
+}
---
+++ src/main/java/com/treeaxes/Orion/Model/Usuario.java
@@ -0,0 +1,11 @@
+package com.treeaxes.Orion.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+
+}
