554ec3343f7830d6f49841ad98268e78e77680ee
RafaxWolf <rafalisandroflores@gmail.com>
2026-08-13 20:15:53 -0400

    haciendo cambios

--- src/main/java/com/treeaxes/Orion/Controller/UserController.java
+++ src/main/java/com/treeaxes/Orion/Controller/UserController.java
@@ -1,6 +1,8 @@
 package com.treeaxes.Orion.Controller;
 
+import com.treeaxes.Orion.Model.Usuario;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/usuarios")
 public class UserController {
-    
+
+    @GetMapping("/alo")
+    public String alo() {
+        return "Hola Buenaaassss";
+    }
 
 }
--- src/main/java/com/treeaxes/Orion/Model/Usuario.java
+++ src/main/java/com/treeaxes/Orion/Model/Usuario.java
@@ -3,9 +3,22 @@ package com.treeaxes.Orion.Model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Entity
-@Table(name = "Usuario")
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(unique = true, nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
 }
